aba33d731107f5c2f7dad17fd6b9192984548c5e
James Awesome <awesome@shopkeep.com>
2016-03-08 14:56:48 -0500

    do not try and re-install

--- recipes/debian.rb
+++ recipes/debian.rb
@@ -1,10 +1,13 @@
-remote_file "#{Chef::Config[:file_cache_path]}/sumocollector.deb" do
-  source node['sumologic']['collectorDEBUrl']
-end
 
-dpkg_package 'sumocollector' do
-  source "#{Chef::Config[:file_cache_path]}/sumocollector.deb"
-  action :install
+unless ::File.exists?(node['sumologic']['installDir'])
+  remote_file "#{Chef::Config[:file_cache_path]}/sumocollector.deb" do
+    source node['sumologic']['collectorDEBUrl']
+  end
+
+  dpkg_package 'sumocollector' do
+    source "#{Chef::Config[:file_cache_path]}/sumocollector.deb"
+    action :install
+  end
 end
 
 service 'collector' do
